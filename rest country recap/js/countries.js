8d8d7a4a5dd1b0867e0fc6b5d4a3aa27019f093e
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-03-21 00:30:25 +0600

    design done

--- rest country recap/js/countries.js
+++ rest country recap/js/countries.js
@@ -1,4 +1,22 @@
 const loadCountries = () =>{
-    fetch('https:://rrestcountries.com/v3.1/all')
-    .then()
+    fetch(`https://restcountries.com/v3.1/all`)
+    .then(res=>res.json())
+    .then(data =>dsplayCountries(data));
 }
+const dsplayCountries = countries =>{
+    console.log(countries[0]);
+    const countriesHTML = countries.map(country => getCountryHTML(country));
+    // console.log(countriesHTML[1]);
+    const container = document.getElementById('countries');
+    container.innerHTML = countriesHTML.join(' ');
+
+}
+
+const getCountryHTML = country =>{
+    return `
+    <div class="country">
+        <h2>${country.name.common}</h2>
+        <img src="${country.flags.png}">
+    </div>`
+}
+loadCountries();
