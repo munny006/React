db7db358bc9f97301720a2f42524e172e86278c5
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-03-22 02:06:18 +0600

    with button

--- reactjs-corer-cncept2/src/App.js
+++ reactjs-corer-cncept2/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
@@ -11,12 +12,13 @@ function App() {
 
   return (
     <div className="App">
-      {
+      {/* {
         products.map(product=> <Product name={product.name} price={product.price}></Product>)
-      }
+      } */}
       {/* <Product name="laptop"price="16000"></Product>
       <Product name="mobile" price="56000"></Product>
       <Product name="Tv"price="6000"></Product> */}
+      <Counter></Counter>
       
     </div>
   );
@@ -26,6 +28,22 @@ function App() {
       <div className="product">
         <h3>Name : {props.name}</h3>
         <p>Price : {props.price}</p>
+      </div>
+    )
+  }
+
+  function Counter(){
+    const [count,setCount] = useState(55);
+    const IncreaseCount = () =>{
+      const newCount = count +1;
+      setCount(newCount);
+    }
+
+    // console.log(abc)
+    return (
+      <div>
+        <h1>Count :{count} </h1>
+        <button onClick={IncreaseCount}>Increase </button>
       </div>
     )
   }
