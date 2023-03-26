51a231f9f44ba79678230082fa3f545fb8dbd6cf
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-03-27 02:38:38 +0600

    complete

--- react-rest-countries/src/components/Countries/Countries.js
+++ react-rest-countries/src/components/Countries/Countries.js
@@ -3,23 +3,24 @@ import Country from '../Country/Country';
 import './Countries.css'
 
 const Countries = (props) => {
-    const [countries,setCountries] = useState([]);
-    useEffect(()=>{
+    const [countries, setCountries] = useState([]);
+    useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
-        .then(res=>res.json())
-        .then(data => setCountries(data));
-    },[])
+            .then(res => res.json())
+            .then(data => setCountries(data));
+    }, [])
     return (
         <div>
-            <h1>Hello From Country : {countries.length}</h1>
-            {/* {
-                countries.map(country =>console.log(country))
-            } */}
-
-{
-                countries.map(country =><Country 
-                    country={country}></Country>)
+            <h1>Hello From Countries : {countries.length}</h1>
+                <div className='countries-container'>
+            {
+                countries.map(country => <Country
+                    country={country}
+                        key={country.cca3}>
+                    </Country>)
             }
+           
+           </div>
         </div>
     );
 };
--- react-rest-countries/src/components/Country/Country.js
+++ react-rest-countries/src/components/Country/Country.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import './Country.css'
 
 const Country = (props) => {
     // const {area,region,population,name} = props.country;
-    const {area,region,name} = props.country;
+    const {area,region,name,flags} = props.country;
+
     return (
         <div className='country'>
             {/* <h2> Country Name : {name.common} </h2>
@@ -11,6 +13,7 @@ const Country = (props) => {
             <p><small>Area : {area}</small></p> */}
 
             <h2> Country Name : {name.common} </h2>
+            <img src={flags.png} alt=""/>
             <p>Area: {area}</p>
             <p><small>Region : {region}</small></p>
             
