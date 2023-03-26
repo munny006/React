2abda529622d5585e080504ca05f6de9d6ac2cb9
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-03-26 21:41:24 +0600

    component

--- react-rest-countries/src/App.js
+++ react-rest-countries/src/App.js
@@ -1,18 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
      <LoadCountries></LoadCountries>
     </div>
-  );
+  )
 }
 
 function LoadCountries(){
+
+  const [countries,setCountries] = useState([]);
+  useEffect(()=>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res=>res.json())
+    .then(data=>setCountries(data))
+  },[])
   return(
     <div>
       <h1>Visiting Every Country of the world !!</h1>
+      <h3>Available countries : {countries.length}</h3>
     </div>
   )
 }
