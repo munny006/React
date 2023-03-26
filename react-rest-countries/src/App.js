9d4d584b9e7dff95cab7c099cb69082c42197a46
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-03-26 21:32:44 +0600

    rest country

---
+++ react-rest-countries/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <LoadCountries></LoadCountries>
+    </div>
+  );
+}
+
+function LoadCountries(){
+  return(
+    <div>
+      <h1>Visiting Every Country of the world !!</h1>
+    </div>
+  )
+}
+
+export default App;
