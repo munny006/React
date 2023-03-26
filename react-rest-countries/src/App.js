6c299231fb5f05447cde785dc4bebd22922c5240
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-03-26 22:53:56 +0600

    component

--- react-rest-countries/src/App.js
+++ react-rest-countries/src/App.js
@@ -22,6 +22,18 @@ function LoadCountries(){
     <div>
       <h1>Visiting Every Country of the world !!</h1>
       <h3>Available countries : {countries.length}</h3>
+      {
+        countries.map(country =><Country name={country.name.common} population = {country.population}></Country>)
+      }
+    </div>
+  )
+}
+
+function Country(props){
+  return (
+    <div>
+      <h2> Name : {props.name}</h2>
+      <p> Populaton : {props.population}</p>
     </div>
   )
 }
