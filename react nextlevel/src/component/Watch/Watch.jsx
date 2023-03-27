b13352aecab438039c52ea851f8e5b9235580aeb
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-03-28 01:25:31 +0600

    nextstep

--- react nextlevel/src/component/Watch/Watch.jsx
+++ react nextlevel/src/component/Watch/Watch.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
 
 const Watch = () => {
-    const [steps,Steps] = useState(0);
-    
+    const [steps,setSteps] = useState(0);
+    const increaseSteps = () =>{
+       const nextSteaps = steps + 1;
+       setSteps(nextSteaps);
+    }
     return (
         <div>
             <h2>This is my Smart Watch .. </h2>
             <p>Steps : {steps} </p>
-            <button>De Dour .....</button>
+            <button onClick={increaseSteps}>De Dour .....</button>
         </div>
     );
 };
