77ba7b9e1c161f3f4d0ef89e7fbcc455cdf0c2ec
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-03-28 01:32:51 +0600

    useeffect

--- react nextlevel/src/component/Watch/Watch.jsx
+++ react nextlevel/src/component/Watch/Watch.jsx
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Watch = () => {
     const [steps,setSteps] = useState(0);
     const increaseSteps = () =>{
        const nextSteaps = steps + 1;
        setSteps(nextSteaps);
+      
     }
+    useEffect(()=>{
+        console.log(steps)
+    },[steps])
     return (
         <div>
             <h2>This is my Smart Watch .. </h2>
