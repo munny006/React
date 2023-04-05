a3d419e299eab90247a4b103b5d4e3960ce5aeea
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-04-06 01:26:12 +0600

    design done

--- price-list-cards/src/App.jsx
+++ price-list-cards/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import PriceList from './components/PriceLists/PriceList'
 import DeashBoard from './components/DeashBoard/DeashBoard'
+import PhoneBar from './components/PhoneBar/PhoneBar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,7 @@ function App() {
       <h1 className='text-7xl text-purple-600'>Hello From Tailwind</h1>
     <PriceList></PriceList>
     <DeashBoard></DeashBoard>
+    <PhoneBar></PhoneBar>
     </div>
   )
 }
