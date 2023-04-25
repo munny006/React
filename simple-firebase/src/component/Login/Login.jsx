b1e6e69ec270b4e5c222a1b948feb871c6bbf72b
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-04-25 19:51:42 +0600

    display loggend in user information signout

--- simple-firebase/src/component/Login/Login.jsx
+++ simple-firebase/src/component/Login/Login.jsx
@@ -1,27 +1,54 @@
-import React from 'react';
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import React, { useState } from 'react';
+import { GoogleAuthProvider, getAuth, signInWithPopup ,signOut } from "firebase/auth";
 import app from '../../firebase/firebase.init';
 
 
 const Login = () => {
+
+    const [user, setUser] = useState(null);
+
     const auth = getAuth(app);
     console.log(app);
     const provider = new GoogleAuthProvider();
 
     const handleGoogleSignIn = () => {
         // console.log('google mama is coming');
-        signInWithPopup(auth,provider)
-        .then(result =>{
-            const user= result.user;
-            console.log(user);
-        })
-        .catch(error => {
-            console.log('error',error.message);
-        })
+        signInWithPopup(auth, provider)
+            .then(result => {
+                const loggedInUser = result.user;
+                console.log(loggedInUser);
+                setUser(loggedInUser);
+            })
+            .catch(error => {
+                console.log('error', error.message);
+            })
     }
+
+    const handleSignOut = () => {
+
+
+        signOut(auth)
+            .then(result => {
+                console.log(result);
+                setUser(null);
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
+
+
     return (
         <div>
             <button onClick={handleGoogleSignIn}>Google Login</button>
+            <button onClick={handleSignOut}>Sign out</button>
+            {user && <div>
+                <h3>User : {user?.displayName}</h3>
+                <p>email:{user.email}</p>
+                <img src={user.photoURL}></img>
+            </div>
+            }
         </div>
     );
 };
