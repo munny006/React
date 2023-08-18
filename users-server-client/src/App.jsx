58ff27ae1020a4cc746a960aa183ca3366a9bedf
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-08-19 01:21:49 +0600

    client site form created

--- users-server-client/src/App.jsx
+++ users-server-client/src/App.jsx
@@ -11,11 +11,33 @@ function App() {
     .then(data =>setUsers(data));
   },[])
 
+
+  //button work
+  const handleAddUser =event =>{
+    event.preventDefault();
+    const form = event.target;
+    const name =  form.name.value;
+    const email =form.email.value;
+    const password =form.password.value;
+    console.log(name,email,password)
+  }
+
   return (
     <div className="App">
      
       <h1>Users Server system</h1>
       <h3>Numbers of Users : {users.length}</h3>
+      <form onSubmit={handleAddUser}>
+        <input type='text' name='name'/><br/><br/>
+        <input type='email' name='email'/><br/><br/>
+        <input type='password' name='password'/><br/><br/>
+        <input type='submit' value="Add User" />
+      </form>
+      <div>
+        {
+          users.map(user => <p key = {user.id}>{user.id} :  {user.name}: {user.email}</p>)
+        }
+      </div>
      
     </div>
   )
