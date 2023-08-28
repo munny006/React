f207da5177f9a049aa28729d3cd5ed4830275e00
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-08-29 00:45:09 +0600

    read data find find multiple user data and display on ul

--- simle-crud-client/src/main.jsx
+++ simle-crud-client/src/main.jsx
@@ -6,6 +6,7 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Users from './components/users';
 
 
 const router = createBrowserRouter([
@@ -13,6 +14,12 @@ const router = createBrowserRouter([
     path: "/",
     element:<App></App>
   },
+  {
+    path:"/users",
+    element:<Users></Users>,
+    loader: () => fetch('http://localhost:5000/users')
+  }
+
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
